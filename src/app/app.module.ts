78f3a178459b4fdcea5c64dcccacbc4b352beb38
tobyschaeffer <toby.schaeffer@gmail.com>
2017-06-21 16:06:42 +0100

    added todos for guarding routes#

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,33 @@ import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 // TODO(1.2) import route in app.module.ts
 // import { RouterModule } from '@angular/router';
-import { StoreFirstGuard } from './storeFirst.guard';
+// TODO(4.2.1) import guard file in app.module.ts
+// import { StoreFirstGuard } from './storeFirst.guard';
 
 @NgModule({
   imports: [BrowserModule, StoreModule,
     // TODO(1.3) Add each route in app.module.ts
     // RouterModule.forRoot([
-    //   { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
-    //   { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard] },
-    //   { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard] },
+    //   {
+    //     path: 'store', component: StoreComponent,
+    //     // TODO(4.2.2) register guard file for each route - store component
+    //     // canActivate: [StoreFirstGuard]
+    //   },
+    //   {
+    //     path: 'cart', component: CartDetailComponent,
+    //     // TODO(4.2.3) register guard file for each route - CartDetail component
+    //     // canActivate: [StoreFirstGuard]
+    //   },
+    //   {
+    //     path: 'checkout', component: CheckoutComponent,
+    //     // TODO(4.2.4) register guard file for each route - Checkout component
+    //     // canActivate: [StoreFirstGuard]
+    //   },
     //   { path: '**', redirectTo: '/store' }
     // ])
 ],
-  providers: [StoreFirstGuard],
+  // TODO(4.2.5) register guard file as provider
+  // providers: [StoreFirstGuard],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- src/app/storeFirst.guard.ts
+++ src/app/storeFirst.guard.ts
@@ -1,22 +1,24 @@
-import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot, RouterStateSnapshot, Router
-} from '@angular/router';
-import { StoreComponent } from './store/store.component';
+// TODO(4.1) create guard file
 
-@Injectable()
-export class StoreFirstGuard {
-  private firstNavigation = true;
-  constructor(private router: Router) { }
-  canActivate(route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot): boolean {
-    if (this.firstNavigation) {
-      this.firstNavigation = false;
-      if (route.component !== StoreComponent) {
-        this.router.navigateByUrl('/');
-        return false;
-      }
-    }
-    return true;
-  }
-}
+// import { Injectable } from '@angular/core';
+// import {
+//   ActivatedRouteSnapshot, RouterStateSnapshot, Router
+// } from '@angular/router';
+// import { StoreComponent } from './store/store.component';
+//
+// @Injectable()
+// export class StoreFirstGuard {
+//   private firstNavigation = true;
+//   constructor(private router: Router) { }
+//   canActivate(route: ActivatedRouteSnapshot,
+//   state: RouterStateSnapshot): boolean {
+//     if (this.firstNavigation) {
+//       this.firstNavigation = false;
+//       if (route.component !== StoreComponent) {
+//         this.router.navigateByUrl('/');
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
