0c6ccc660603a2d6ee5fcb198bcff073fd897c22
tobyschaeffer <toby.schaeffer@gmail.com>
2017-06-21 17:00:50 +0100

    Completed Step 1 - create and apply routing for each component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  // TODO(1.4) - define routing target in app.component.ts
-  // template: '<router-outlet></router-outlet>'
-template: '<store></store>'
+  // TODO(1.4) - define routing target in app.component.ts - add new target
+  template: '<router-outlet></router-outlet>'
+  // TODO(1.4) - define routing target in app.component.ts - delete old targe
+// template: '<store></store>'
 })
 export class AppComponent {
 }
