a3b4908b800e7a531f5e06035f17519f18bd2573
tobyschaeffer <toby.schaeffer@gmail.com>
2017-06-21 14:01:23 +0100

    updated with routing

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'store',
@@ -12,8 +14,9 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) {}
-
+  constructor(private repository: ProductRepository,
+              private cart: Cart,
+              private router: Router) {}
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
     return this.repository.getProducts(this.selectedCategory)
@@ -32,10 +35,13 @@ export class StoreComponent {
     this.productsPerPage = Number(newSize);
     this.changePage(1);
   }
-
   get pageCount(): number {
     return Math.ceil(this.repository
         .getProducts(this.selectedCategory).length / this.productsPerPage);
+  }
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl('/cart');
   }
 
   // get pageNumbers(): number[] {
